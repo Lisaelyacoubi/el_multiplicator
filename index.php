7c956ff5a8e77627f1a3b1b653b683b0ee18dd77
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-07-09 22:31:06 +0200

    starting revision

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   </head>
   <body>
+    <?php
+    session_start();
+    $_SESSION["newsession"] = $value;
+
+     ?>
     <section class="container d-flex justify-content-center pt-4">
       <!--Card table multiplication -->
       <div class="card text-center" style="width: 18rem;">
@@ -32,7 +37,47 @@
               }
             ?>
         </div>
-      </div>
       <!--Fin card table multiplication -->
+      </div>
+      <!--Révision table de multiplication -->
+      <div class="container">
+        <div class="accordion" id="accordionExample">
+          <div class="card">
+            <div class="card-header" id="headingOne">
+              <h2 class="mb-0">
+                <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Réviser les tables de multiplication
+                </button>
+              </h2>
+            </div>
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+              <div class="card-body">
+                <form class="form-control border-0" action="index.php" method="post">
+                  <select class="border-0 bg-primary text-white" name="revision">
+                    <?php
+                      for($i = 1; $i <= 10; $i++){
+                        echo '<option value="'.$i.'"> Table ' . $i . '</option>';
+                      }
+                    ?>
+                  </select>
+                  <?php
+                    if(isset($_POST['revision'])){
+                      $revision_multiplication = $_POST['revision'];
+                      $random_number = rand(1, 10);
+                      echo $revision_multiplication . " X " . $random_number ." = " . " ?";
+                    }
+                  ?>
+                  <input type="submit" value="envoyer">
+                </form>
+
+                <!--form  + input qui récupère la réponse -->
+                <!--comparer la réponse et le résultat -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--Fin révision table de multiplication -->
+    </section>
   </body>
 </html>
