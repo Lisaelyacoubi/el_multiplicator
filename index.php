c9a12f076a99bc46db576445f2e6d69153889c83
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-07-09 11:28:50 +0200

    add style with bootstrap

--- index.php
+++ index.php
@@ -8,36 +8,31 @@
   </head>
   <body>
     <section class="container d-flex justify-content-center pt-4">
-      <!--Table du 3 -->
+      <!--Card table multiplication -->
       <div class="card text-center" style="width: 18rem;">
         <div class="card-body bg-primary">
           <h5 class="card-title text-center pb-3">Table de multiplication</h5>
-            <form class="form-control" action="index.php" method="post">
-              <select class="" name="number_multiplication">
+            <form class="form-control bg-primary mb-2 border-0" action="index.php" method="post">
+              <select class="bg-primary border-0 text-white" name="number_multiplication">
                 <?php
                   for($i = 1; $i <= 10; $i++){
                     echo '<option value="'.$i.'"> Table ' . $i . '</option>';
                   }
-                 ?>
+                ?>
               </select>
-              <input type="submit" name="" value="envoyer">
+              <input type="submit" name="" value="envoyer" class="btn btn-sm btn-dark">
             </form>
-          </div>
-        </div>
-
             <?php
               if(isset($_POST['number_multiplication'])){
                 $multiplication = $_POST['number_multiplication'];
                 for($i = 1; $i<= 10; $i++){
-
                   $sum = ($multiplication * $i);
                   echo $multiplication . " X ". $i . " = ". $sum . "<br>";
                 }
               }
-
-             ?>
-
-
-
+            ?>
+        </div>
+      </div>
+      <!--Fin card table multiplication -->
   </body>
 </html>
